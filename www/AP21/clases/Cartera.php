7424a02d4f75f03e181d9e218f7b1c66355bd198
Jorge Esteve Pascual <joespa@floridauniversitaria.es>
2024-03-12 11:48:24 +0100

    añadiendo el archivo sistema login

--- www/AP21/clases/Cartera.php
+++ www/AP21/clases/Cartera.php
@@ -1,111 +1,73 @@
 <?php
-class Cartera{
-private $clientes=[];
-private $fichero;
 
-function __construct($fichero){
-    $this->fichero=$fichero;
-    $this-> loadData($fichero);
-}
-public function loadData($fichero)
+class Cartera
+{
+    private $clients = [];
+
+
+    public function getClient($id)
     {
-        $gestor = fopen($fichero, "r");
-        while (($element = fgetcsv($gestor)) !== false) {
-            array_push(
-                $this->clientes,
-                new Empresa(...$element) //Spread Operator
-            );
+        foreach ($this->clients as $client) {
+            if ($client->getId() == $id)
+                return $client;
         }
-        fclose($gestor);
+        return new Empresa(null, null, null, null, null);
     }
 
+   
 
-	public function isVIP($investment){
-        return $investment >= 1000000;
-    }
-    public function drawlist()
+    public function delete($id)
     {
-        $html = "";
-        foreach ($this->clientes as $Cliente) {
-            $styleClass = '';
-
-            if ($this->isVIP($Cliente->getInvestment())) {
-                $styleClass = 'vip';
-            }
-
-            $html .= '<tr ' . $styleClass . '>';
-            $html .= '<td>' . $Cliente->getId() . '</td>';
-            $html .= '<td class=' . $styleClass . '>' . $Cliente->getCompany() .'</td>';
-            $html .= '<td>' . $Cliente->getInvestment() . '€' . '</td>';
-            $html .= '<td>' . $Cliente->getDate() . '</td>';
-
-           
-            if ($Cliente->getActive() == 'True') {
-                $html .= '<td><img src="img/img05.gif"></td>';
-            } else {
-                $html .= '<td><img src="img/img06.gif" ></td>';
-            }
-            $html .= '<td><a href="delete.php?id=' . $Cliente->getId() . '"><img src="img/del_icon.png" width="25"></a></td>';
-
-            $html .= '<td><a href="edit.php?id=' . $Cliente->getId() . '"><img src="img/edit_icon.png" width="25"></a></td>';
-            $html .= '<td><a href="insert.php?id=' . $Cliente->getId() . '"><img src="img/ins_icon.png" width="25"></a></td>';
-            $html .= '</td>';
-            $html .= '</tr>';
-        }
-        return $html;
-    }
-    function delete($idEmpresa){
-        for ($i = 0; $i < count($this->clientes); $i++){
-            if($this->clientes[$i]->getId() == $idEmpresa){
-                array_splice($this->clientes, $i, 1);
-            }
-        }
-        $this->persist();
-    }
-    
-    function persist(){
-    $gestor = fopen("data.csv", "w");
-    foreach ($this->clientes as $cliente ) {
-
-        fputcsv($gestor,[
-            $cliente->getId(),
-            $cliente->getCompany(),
-            $cliente->getInvestment(),
-            $cliente->getDate(),
-            $cliente->getActive()
-        ]);
-    
-        }
-
-        fclose($gestor);
-
+       
     }
 
-    function getClient($id){
-        foreach ($this->clientes as $cliente) {
-            if($cliente->getId()==$id){
-                return $cliente;
-            }
-        }
-    }
-    function update($datos){
-
-        foreach ($this->clientes as $client) {
-            if ($client->getId() == $datos["id"]) {
-                $client->setCompany($datos["company"]);
-                $client->setInvestment($datos["investment"]);
-                $client->setDate($datos["date"]);
-                $client->setActive($datos["active"]);
-            }
-        }
-        $this->persist();
-    }
-    function insert($datos){
-        $clase=new Empresa($datos["id"],$datos["company"],$datos["investment"],$datos["date"],$datos["active"]);
+    public function update($datos)
+    {
         
-        array_push($this->clientes,$clase);
-        
-        $this->persist();
     }
 
+   
+
+    public function insert($datos)
+    {
+    
+       
+    }
+
+    
+
+    public function drawList()
+    {
+        $conn = mysqli_connect('db', 'root', 'test', "AP21");
+                
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $query = 'SELECT * From investment';
+        $result = mysqli_query($conn, $query);
+
+        echo '<table class="table table-striped">';
+        echo '<tr>
+                <th>Id</th>
+                <th>Company</th>
+                <th>Investment</th>
+                <th>Date</th>
+                <th>Active</th>
+                <th colspan="2">Actions</th>
+            </tr>';
+        while($value = $result->fetch_array(MYSQLI_ASSOC)){
+            echo '<tr>';
+            foreach($value as $element){
+                echo '<td>' . $element . '</td>';
+            }
+
+            echo '</tr>';
+        }
+        echo '</table>';
+
+        $result->close();
+        mysqli_close($conn);
+    }
 }
+?>
--- www/AP21/lista.php
+++ www/AP21/lista.php
@@ -1,44 +1,35 @@
 <?php
-require_once "autoloader.php";
-$cartera= new Cartera('data.csv');
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-$client = $cartera->getClient($id);
+
+require_once "autoload.php";
+
+$cartera = new Cartera();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    
 </head>
+
 <body>
     <table class="redTable">
         <thead>
             <tr>
-                <th>Id</th>
-                <th>Company</th>
-                <th>Investment</th>
-                <th>Date</th>
-                <th>Active</th>
-                <th colspan="3">Actions</th>
-            </tr>
-        </thead>
-        <tfoot>
-            <tr>
-                <td colspan="8">
-
+                <td colspan="7">
+                    <div> <a href="new.php">New Client</a> </div>
                 </td>
             </tr>
-        </tfoot>
+        </thead>
         <tbody>
-			<?=$cartera->drawlist()?>
+            <?= $cartera->drawList() ?>
         </tbody>
     </table>
-
-
 </body>
+
 </html>
